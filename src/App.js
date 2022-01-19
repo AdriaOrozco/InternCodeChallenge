3a7a15f75f3c1b2e47a11aa7cc40044c1d2607df
1490952 <adria.orozco@e-campus.uab.cat>
2022-01-19 01:56:31 +0100

    Finalizacion

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import './index';
 import React, {Component} from 'react';
 
+
 class App extends Component {
     render() {
         return (
@@ -25,19 +26,20 @@ class App extends Component {
 }
 const Canciones = ({canciones}) => {
     return (
-        <div class="FondoCanciones">
-            <h1 id="Titulo">Canciones</h1>
+        <div className="FondoCanciones">
+                <h1 id="Titulo">Canciones</h1>
             <table>
-                <tr>
-                    <th colSpan="2">Título</th>
-                </tr>
+                <tr><th id="col1" colSpan="2">TÍTULO</th>
+                    <th id="col2">ÁLBUM</th>
+                    <th id="col3">DURACIÓN</th></tr>
+                <br></br>
                 {canciones.map((canciones) => (
-                <tr id={canciones.id}>
-                    <td><img src={canciones.album.images[2].url} /></td>
-                    <td>
-                        <tr>{canciones.name}</tr>
-                        <tr>{mostrarArtistas(canciones)}</tr>
+                <tr id={canciones.id}><td><img src={canciones.album.images[2].url} /></td>
+                    <td className="NombreCanciones"><tr id="nombreCancion">{canciones.name}</tr>
+                        <tr id="artist">{mostrarArtistas(canciones)}</tr>
                     </td>
+                    <td id="album">{canciones.album.name}</td>
+                    <td id="duration">{pasoASec(canciones.duration_ms)}</td>
                 </tr>
                 ))}
             </table>
@@ -52,9 +54,13 @@ function mostrarArtistas(canciones){
             cadena=canciones.artists[i].name;
         }else {
             cadena = cadena + ", " + canciones.artists[i].name;
-        }
-    }
+        }}
     return cadena;
+}
+function pasoASec (millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return `${minutes}:${(seconds < 10 ? "0" : "")}${seconds}`;
 }
 
 export default App;
